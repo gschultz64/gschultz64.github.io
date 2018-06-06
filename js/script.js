6f10f08218bae109b27a0bf6461b80c35d7cf79f
Genevieve Schultz <genschultz@gmail.com>
2018-06-06 16:36:12 -0700

    updated project 1 files

--- js/script.js
+++ js/script.js
@@ -1,30 +1,102 @@
-$(document).ready(function() {
+var colors = [
+  '#3C989E', '#5DB5A4', '#F4CDA5', '#F57A82', '#E37B40', '#5E005E', '#AB2F52', '#41733F'
+];
+var squares = [
+  $('#box1'), $('#box2'), $('#box3'), $('#box4'),
+  $('#box5'), $('#box6'), $('#box7'), $('#box8'),
+  $('#box9'), $('#box10'), $('#box11'), $('#box12'),
+  $('#box13'), $('#box14'), $('#box15'), $('#box16')
+];
+// New empty array that will hold 2 copies of each color code from colors array
+var moreColors = [];
+moreColors.push(colors);
+moreColors.push(colors);
+var randomColors = moreColors.reduce(function(accumulator, currentValue) {
+  return accumulator.concat(currentValue);
+})
+
+console.log(randomColors);
+
+
+//Shuffle colors in array randomColors
+Array.prototype.shuffle = function () {
+  var input = this;
+
+  for (var i = input.length - 1; i >= 0; i--) {
+
+    var randomIndex = Math.floor(Math.random() * (i + 1));
+    var itemAtIndex = input[randomIndex];
+
+    input[randomIndex] = input[i];
+    input[i] = itemAtIndex;
+  }
+  return input;
+}  
+
+
+// For loop to randomly add one color for each item from squares array (each square on the board)
+// for (let i = 0; i < squares.length; i++) {
+//   randomColors[i] = colors[Math.floor(Math.random() * colors.length)];
+// }
+// console.log(randomColors);
+// // Limit 2 of each color being put into randomColors
+// $.inArray()
+
+
+$(document).ready(function () {
   // Initialize Materialize components
-  M.AutoInit();
-  // click on square and change the color
-  $(".col").click(function () {
-    $(this).css("background-color", "rebeccapurple");
-  });
-  $.get("http://thecolorapi.com/id?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html", function () {
-    console.log("api requested");
-  }).done(function (returnData) {
-    console.log(returnData);
-  });
+  M.AutoInit(); 
+  
+  // flipSquare();
+  
   //default audio/background music?
+
+  // Button Clicks
+  // Solitaire Button Click
+  $('#solitaire').click(function () {
+    // Set game to play alone
+    // Display Solitaire rules
+    $('p').text("Click to turn over any two squares. If the two squares match, you win! If they don't match, click another square. Repeat as many times as you want!");
+    // $('.modal').modal('open');
+  });
+  // PvP Button Click
+  $('#twoPlayers').click(function () {
+    // Set game to play against another player
+    // Display PvP rules
+    $('p').text("Player 1 starts: Click to turn over any two squares. If the two squares match, you win! If they don't match, Player 2 takes a turn to click two squares. Repeat as many times as you want!");
+  });
+  // PvAi Button Click
+  $('#computer').click(function () {
+    // Set game to play against computer/ai
+    // Display PvAi rules
+    $('p').text("Click to turn over any two squares. If the two squares match, you win! If they don't match, the computer takes a turn to click two squares. Repeat as many times as you want!");
+  });
+  // Reset Button
+  $('#autorenew').click(function () {
+    $('.col').css('background-color', '');
+  });
+
 });
 
-// function flipSquare() {
-  // AJAX query for color API
+function flipSquare() {
+  
+  // click on square and change the color
+  $('.col').click(function () {
+      $(this).css("background-color", );
+  });
+}
 
-  // $.get("http://thecolorapi.com/scheme{mode=monochrome-dark&count=8}");
+
+
+
   // Display color on div
   // play sound effect?
   // use flipSquare in "click" event
-// }
+
 
 // function checkForMatch() {
-//   // Check for matching colors when two squares are "flipped"
-  
+  // Check for matching colors when two squares are "flipped"
+
 // }
 
 // change game instructions in .gameInstruct depending on game mode selected
@@ -36,7 +108,4 @@ $(document).ready(function() {
 //  Player vs. Player Mode
 //  Player vs. AI Mode
 
-// Reset Button
-// function reset(e) {
-  
-// }
+
